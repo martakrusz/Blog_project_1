52bfaae3123d4aa7981591bf899f5a6bb8bc9d7e
Marta Kruszewska <martakruszewska007@gmail.com>
2020-10-24 13:24:22 +0200

    edit form

--- blog/routes.py
+++ blog/routes.py
@@ -10,33 +10,37 @@ def index():
       
       return render_template("homepage.html", all_posts=all_posts)
 
-@app.route("/new-post/", methods=["GET","POST"])
-def create_entry():
-      form = EntryForm()
-      errors = None
-      if request.method == 'POST':
-            if form.validate_on_submit():
-                  entry = Entry(
-                        title=form.title.data,
-                        body=form.body.data,
-                        is_published=form.is_published.data
-                        )
-                  db.session.add(entry)
-                  db.session.commit()
-                  flash('You were successfully add post')
-            else:
-                  errors = form.errors
-      return render_template("entry_form.html", form=form, errors=errors)
 
-@app.route("/edit-post/<int:entry_id>", methods=["GET","POST"])
-def edit_entry(entry_id):
+@app.route("/new-post/<int:entry_id>", methods=["GET","POST"])
+def create_edit_entry(entry_id = 0):
+   errors = None
+   if entry_id == 0:
+      form = EntryForm()
+   else:
       entry = Entry.query.filter_by(id=entry_id).first_or_404()
       form = EntryForm(obj=entry)
-      errors = None
-      if request.method == 'POST':
-            if form.validate_on_submit():
-                  form.publicate_obj(entry)
-                  db.session.commit()
+
+   if request.method == "POST":
+      if request.form["btn"] == "Save":
+         if form.validate_on_submit():
+            if entry_id == 0:
+               entry = Entry(
+                              title = form.title.data,
+                              body = form.body.data,
+                              is_published =form.is_published.data
+                              )
+               db.session.add(entry)
             else:
-                  errors = form.errors
-      return render_template("entry_form.html", form=form, errors=errors)
+               form.populate_obj(entry)
+         else:
+            errors = form.errors
+            flash('Nie wpowadzono zmian', 'danger')
+
+      if request.form["btn"] == "Cancel":
+         return redirect(url_for("index"))
+
+      db.session.commit()
+      flash('Zmiany dodane pomyślnie', 'success')
+      return redirect(url_for("index"))
+
+   return render_template("entry_form.html", form=form, errors=errors)
