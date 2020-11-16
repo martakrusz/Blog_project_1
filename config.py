88e9492530aed526372a642865d7e9429d36a443
Marta Kruszewska <martakruszewska007@gmail.com>
2020-11-16 20:18:19 +0100

    add wtf

--- config.py
+++ config.py
@@ -3,11 +3,13 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY="powerful secretkey"
+    SECRET_KEY=os.urandom(24)
     WTF_CSRF_SECRET_KEY="a csrf secret key"
     SQLALCHEMY_DATABASE_URI = (
-        os.environ.get('DATABASE_URL') or
-        'sqlite:///' + os.path.join(BASE_DIR, 'blog.db')
+        os.environ.get("DATABASE_URL") or
+        "sqlite:///" + os.path.join(BASE_DIR, "blog.db")
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ADMIN_USERNAME=os.environ.get("ADMIN_USERNAME", "admin")
+    ADMIN_PASSWORD=os.environ.get("ADMIN_PASSWORD", "admin")
 
